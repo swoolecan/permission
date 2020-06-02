06ed344cdccfa348d2e29822ea9fa243082fadb0
wangcanliang <wangcanliang@weinaya.com>
2020-06-02 11:09:05 +0800

    develop permission

--- src/Models/Role.php
+++ src/Models/Role.php
@@ -137,4 +137,19 @@ class Role extends Model implements RoleContract
 
         return $this->permissions->contains('id', $permission->id);
     }
+
+    protected function getList(array $params, int $pageSize)
+    {
+        if (!empty($params['with'])) {
+            $query = $this->with($params['with']);
+        } else {
+            $query = $this->query();
+        }
+        if (!isset($params['sort_name']) || empty($params['sort_name'])) {
+            $params['sort_name'] = $this->primaryKey;
+        }
+        $params['sort_value'] = isset($params['sort_value']) ? ($params['sort_value'] == 'descend' ? 'desc' : 'asc') : 'desc';
+        $list = $query->orderBy($params['sort_name'], $params['sort_value'])->paginate($pageSize);
+        return $list;
+    }
 }
